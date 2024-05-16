8fee7f3e2edb736e81c0a080ce78cb6733744afd
JAK <jonathanakidd@gmail.com>
2024-05-15 22:22:34 -0400

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let palindrome = true;
+    let initialArray = [];
+    for (const char of string) {
+        if (char.toUpperCase() != char.toLowerCase()) {
+            initialArray.push(char.toLowerCase())
+        }
+    }
+    let reversedArray = [...initialArray].reverse();
+    for (let i = 0; i < initialArray.length; i++) {
+        if (initialArray[i] != reversedArray[i] ) {
+            palindrome = false;
+            break;
+        }
+    }
+    return palindrome;
 };
 
 // Do not edit below this line
