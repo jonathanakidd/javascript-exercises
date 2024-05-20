2d49c73776011a56eb2db33d7f2d0a3bbe1a6524
JAK <jonathanakidd@gmail.com>
2024-05-20 12:32:55 -0400

    Age function completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
 };
+
+
+
+const age = (birthYear, deathYear) {
+    if (!deathYear) {
+        let currentDate = new Date();
+        deathYear = currentDate.getFullYear();    
+    }
+    return deathYear - birthYear;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
