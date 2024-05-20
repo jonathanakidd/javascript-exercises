90358ef481a4d365ecb83adef40e77240555a1c8
JAK <jonathanakidd@gmail.com>
2024-05-19 21:03:23 -0400

    Finished get the Titles

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,12 @@
 const fibonacci = function(sequence) {
-    sequence = Number(sequence);
-    let numbers = [];
-    for (i = (sequence-1); i < sequence; i++) {
-        if (i < 2) {
-            numbers.push(1);
-        }
-        else {
+    sequence = Number(sequence) -1;
+    console.log(sequence)
+    let numbers = [1,1];
+    for (i = 2; i <= sequence; i++) {
             numbers.push(numbers[i-2] + numbers[i-1]);
         }
-    }
     console.log(numbers);
-    return numbers[sequence -1];
+    return numbers[sequence];
 };
 
 // Do not edit below this line
