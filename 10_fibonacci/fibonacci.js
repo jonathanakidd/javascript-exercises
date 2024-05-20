5f6f49bd06da325b76d11919d7600239e7a300fc
JAK <jonathanakidd@gmail.com>
2024-05-19 20:48:38 -0400

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequence) {
+    sequence = Number(sequence);
+    let numbers = [];
+    for (i = (sequence-1); i < sequence; i++) {
+        if (i < 2) {
+            numbers.push(1);
+        }
+        else {
+            numbers.push(numbers[i-2] + numbers[i-1]);
+        }
+    }
+    console.log(numbers);
+    return numbers[sequence -1];
 };
 
 // Do not edit below this line
